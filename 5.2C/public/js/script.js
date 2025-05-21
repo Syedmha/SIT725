dd0375426b827cd715f84a8e0aa17e69a8346013
Syed <syedabedi12@gmail.com>
2025-05-21 17:07:28 +1000

    Final updates

--- 5.2C/public/js/script.js
+++ 5.2C/public/js/script.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
 
-  // Show toast on success (optional)
   const urlParams = new URLSearchParams(window.location.search);
   if (urlParams.get("submitted") === "true") {
     M.toast({ html: "Project submitted successfully!" });
